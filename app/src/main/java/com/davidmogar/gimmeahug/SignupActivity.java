615ab1a3071e8fea1a064360f813b583f1bee4eb
David Moreno García <david.mogar@gmail.com>
2015-04-15 20:41:19 +0200

    Add material design toolbar to SignupActivity

--- app/src/main/java/com/davidmogar/gimmeahug/SignupActivity.java
+++ app/src/main/java/com/davidmogar/gimmeahug/SignupActivity.java
@@ -1,17 +1,30 @@
 package com.davidmogar.gimmeahug;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class SignupActivity extends ActionBarActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeButtonEnabled(true);
+        }
     }
 
 
@@ -30,7 +43,7 @@ public class SignupActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_done) {
             return true;
         }
 
--- app/src/main/java/com/davidmogar/gimmeahug/WelcomeActivity.java
+++ app/src/main/java/com/davidmogar/gimmeahug/WelcomeActivity.java
@@ -45,7 +45,7 @@ public class WelcomeActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_done) {
             return true;
         }
 
