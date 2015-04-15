3d323a8484d2c24a02751390ed8eccbea81662f7
David Moreno García <david.mogar@gmail.com>
2015-04-15 21:36:15 +0200

    Add methods to handler signUp

--- app/src/main/java/com/davidmogar/gimmeahug/SignupActivity.java
+++ app/src/main/java/com/davidmogar/gimmeahug/SignupActivity.java
@@ -1,11 +1,27 @@
 package com.davidmogar.gimmeahug;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 
 public class SignupActivity extends ActionBarActivity {
@@ -27,6 +43,19 @@ public class SignupActivity extends ActionBarActivity {
         }
     }
 
+    public void handleSignUpData() {
+        String username = ((EditText) findViewById(R.id.username)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password)).getText().toString();
+
+        if (username.trim().length() == 0 || password.trim().length() == 0) {
+            Toast.makeText(getApplicationContext(), getText(R.string.signup_emptyform_error), Toast.LENGTH_SHORT).show();
+        } else if (password.length() < 5) {
+            Toast.makeText(getApplicationContext(), getText(R.string.password_not_secure_error), Toast.LENGTH_SHORT).show();
+        } else {
+//            signUp(username, password);
+            startActivity(new Intent(this, MainActivity.class));
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,11 +71,36 @@ public class SignupActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_done) {
+            handleSignUpData();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void signUp(String username, String password) {
+        HttpClient client = new DefaultHttpClient();
+        HttpGet httpGet = new HttpGet("");
+
+        try {
+            HttpResponse response = client.execute(httpGet);
+            if (response.getStatusLine().getStatusCode() == 200) {
+
+            } else {
+                InputStream content = response.getEntity().getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                StringBuilder builder = new StringBuilder();
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+            }
+        } catch (ClientProtocolException e) {
+
+        } catch (IOException e) {
+
+        }
     }
 }
--- app/src/main/java/com/davidmogar/gimmeahug/WelcomeActivity.java
+++ app/src/main/java/com/davidmogar/gimmeahug/WelcomeActivity.java
@@ -1,11 +1,13 @@
 package com.davidmogar.gimmeahug;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.VideoView;
 
 
@@ -29,27 +31,14 @@ public class WelcomeActivity extends ActionBarActivity {
         });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_done) {
-            return true;
+    public void onSignup(View view) {
+        switch (view.getId()) {
+            case R.id.signup_email:
+                startActivity(new Intent(this, SignupActivity.class));
+                break;
+            case R.id.signup_plus:
+                break;
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
